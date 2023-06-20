ea60817691acdf3028ddb2438d198183dfcde987
Behan Kusi <behan.kusi@edu.tbz.ch>
2023-06-20 12:09:59 +0000

    Delete Methode hinzugefügt

--- LB2/main.js
+++ LB2/main.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 
 let Tasks = [
     {
-        id : 2,
+        id: 2,
         titel: "ueue",
         Erstellungsdatum: "13.06.2023",
         Erfüllungsdatum: "13.06.2023"
@@ -47,10 +47,9 @@ mypwd = "m295";
 app.post("/login", (req, res) => {
     loginmail = req.query.email;
     loginpwd = req.query.password;
-
-    if ((loginmail = mymail) && (loginpwd = mypwd)) {
+    if (loginpwd == mypwd) {
         req.session.authenticated = true;
-        res.send("54");
+        res.status(200).send("user authenticated")
     } else {
         res.sendStatus(401);
     }
@@ -58,7 +57,7 @@ app.post("/login", (req, res) => {
 
 app.get("/verify", (req, res) => {
     if (req.session.authenticated == true) {
-        res.send(mymail);
+        res.status(200).send("authentifiziert");
     } else {
         res.status(401).send("nicht authentifiziert");
     }
@@ -71,62 +70,85 @@ app.delete("/logout", (req, res) => {
 
 app.get("/tasks", (req, res) => {
     if (req.session.authenticated == true) {
-      res.status(200).send(Tasks);
+        res.status(200).send(Tasks);
     } else {
-      res.status(401).send("no auth");
+        res.status(403).send("no auth");
     }
-  });
+});
 
-  app.post("/tasks", (req, res) => {
-    let Newtask = req.body;
-    if (Newtask !== {}) {
-      Tasks.push(Newtask);
-      res.status(200).json(Newtask);
-    } else {
-      res.status(422).send("Leeres Objekt");
-    }
-  });
-
-  app.get("/tasks/:ID", (req, res) => {
+app.post("/tasks", (req, res) => {
     if (req.session.authenticated == true) {
-      let taskobject = {};
-      const id = req.params.ID;
-      taskobject = FindById(Tasks, id);
-      if (!(typeof taskobject === "undefined")) {
-        res.status(200).json(taskobject);
-      } else {
-        res.status(404).send("nicht gefunden");
-      }
-    } else {
-      res.status(401).send("no auth");
+        let Newtask = req.body;
+        if (Newtask !== {}) {
+            Tasks.push(Newtask);
+            res.status(201).json(Newtask);
+        } else {
+            res.status(422).send("Leeres Objekt");
+        }
     }
-  });
-
-  app.put("/tasks/:ID", (req, res) => {
-    const id = req.params.ID;
-    let taskobject = req.body;
-    if (IsNullChecker(NewBook) == true) {
-      let index = Tasks.indexOf(FindById(Books, id));
-      Tasks[index] = { ...Tasks[index], ...taskobject };
-      res.status(200).json(taskobject);
-    } else {
-      res.status(422).send("Property leer");
+    else {
+        res.status(403).send("no auth");
     }
-  });
+});
 
-  function FindById(data, id) {
+app.get("/tasks/:ID", (req, res) => {
+    if (req.session.authenticated == true) {
+        let taskobject = {};
+        const id = req.params.ID;
+        taskobject = FindById(Tasks, id);
+        if (!(typeof taskobject === "undefined")) {
+            res.status(200).json(taskobject);
+        } else {
+            res.status(404).send("nicht gefunden");
+        }
+    } else {
+        res.status(403).send("no auth");
+    }
+});
+
+app.put("/tasks/:ID", (req, res) => {
+    if (req.session.authenticated == true) {
+        const id = req.params.ID;
+        let taskobject = req.body;
+        if (IsNotNullChecker(taskobject) == true) {
+            let index = Tasks.indexOf(FindById(Tasks, id));
+            Tasks[index] = { ...Tasks[index], ...taskobject };
+            res.status(200).json(taskobject);
+        } else {
+            res.status(422).send("Property leer");
+        }
+    }
+    else {
+        res.status(403).send("no auth");
+    }
+});
+
+app.delete("/tasks/:ID", (req, res) => {
+    if (req.session.authenticated == true) {
+        const id = req.params.ID;
+        let index = Tasks.indexOf(FindById(Tasks, id));
+        Tasks.splice(index, 1);
+        res.status(200).send("Erfolgreich");
+    } else {
+        res.status(403).send("no auth");
+    }
+}
+);
+
+function FindById(data, id) {
     for (var i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        return data[i];
-      }
+        if (data[i].id == id) {
+            return data[i];
+        }
     }
-  }
-  
-  function IsNullChecker(report) {
-    return !Object.values(report).every((o) => o === "");
-  }
+}
 
-  app.listen(8080, () => {
+// https://stackoverflow.com/questions/50619910/how-to-check-if-every-properties-in-an-object-are-null
+function IsNotNullChecker(report) {
+    return !Object.values(report).every((o) => o === "");
+}
+
+app.listen(8080, () => {
     console.log("Server is running on port weerew");
-  });
+});
 
